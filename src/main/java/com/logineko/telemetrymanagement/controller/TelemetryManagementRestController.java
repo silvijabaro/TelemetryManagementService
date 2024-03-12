9d86dffc734ed8a0c06ff2f252218191b7fadd14
Silvija Baro <silvija.baro@gmail.com>
2024-03-12 01:08:00 +0100

    Improve Swagger

--- src/main/java/com/logineko/telemetrymanagement/controller/TelemetryManagementRestController.java
+++ src/main/java/com/logineko/telemetrymanagement/controller/TelemetryManagementRestController.java
@@ -2,12 +2,19 @@ package com.logineko.telemetrymanagement.controller;
 
 import com.logineko.telemetrymanagement.filter.FilterService;
 import com.logineko.telemetrymanagement.model.dto.DataFilter;
+import com.logineko.telemetrymanagement.model.dto.FilteredTelemetryResponse;
 import com.logineko.telemetrymanagement.service.TelemetryManagementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +24,7 @@ import java.util.List;
 @Log4j2
 @RequestMapping("/api/v1/telemetry")
 @RequiredArgsConstructor
+@Validated
 public class TelemetryManagementRestController {
 
     private static final String CSV_EXTENSION = "csv";
@@ -24,7 +32,13 @@ public class TelemetryManagementRestController {
     private final FilterService filterService;
 
 
-    @PostMapping("/import")
+    @PostMapping(value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Import telemetry data for Tractors and Combines",
+            responses = {
+                    @ApiResponse(description = "Status of import",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class))),
+                    @ApiResponse(responseCode = "400", description = "If invalid file format is provided")})
     public ResponseEntity<?> importTelemetry(@RequestParam("file") MultipartFile file) throws Exception {
         if (file.isEmpty()) {
             return ResponseEntity.badRequest().body("No CSV file provided.");
@@ -38,7 +52,13 @@ public class TelemetryManagementRestController {
         return ResponseEntity.ok("Telemetry data successfully imported.");
     }
 
-    @PostMapping("/filter")
+    @PostMapping(value = "/filter")
+    @Operation(summary = "Filter telemetry data",
+            responses = {
+                    @ApiResponse(description = "The telemetry data for Tractors and Combines",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = FilteredTelemetryResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid filters were provided")})
     public ResponseEntity<?> filterTelemetry(
             @RequestBody @Valid List<DataFilter> filter) {
         List<String> invalidFields = filterService.validateFilters(filter);
